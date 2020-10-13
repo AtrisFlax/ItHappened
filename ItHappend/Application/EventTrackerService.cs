21db30a5cb876ca90f8d30282421b38d226f0807
Roman Rubtsov <w-raven91@mail.ru>
2020-10-13 22:41:44 +0300

    Add EventTrackerService.cs

---
+++ ItHappend/Application/EventTrackerService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using ItHappend.Domain;
+
+namespace ItHappend.Application
+{
+    public class EventTrackerService
+    {
+        private readonly IEventTrackerRepository _eventTrackerRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IEventRepository _eventRepository;
+
+        public Guid CreateTracker(Guid creatorId)
+        {
+            var creator = _userRepository.LoadUserAuthInfo(creatorId);
+            var newTrackerId = Guid.NewGuid();
+            var eventTracker = new EventTracker(
+                newTrackerId,
+                new List<Event>(),
+                creatorId
+                );
+            return newTrackerId;
+        }
+
+        public void AddEventToTracker(Guid trackerId, Guid eventId)
+        {
+            var requiredTracker = _eventTrackerRepository.LoadEventTracker(trackerId);
+            var eventToAdd = _eventRepository.LoadEvent(eventId);
+            requiredTracker.AddEvent(eventToAdd);
+        }
+
+        public void RemoveEventFromTracker(Guid trackerId, Guid eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTracker(Guid trackerOwnerId, Guid tracker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
