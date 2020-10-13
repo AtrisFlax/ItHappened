c7ff641056703f1fc629733f89d9ce75f20c79ed
Your Name <you@example.com>
2020-10-13 22:30:21 +0300

    Test added AddEventToTracker

--- ItHappend.UnitTests/EventTrackerTests.cs
+++ ItHappend.UnitTests/EventTrackerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using ItHappend.Domain;
 using NUnit.Framework;
 
@@ -7,24 +9,59 @@ namespace ItHappend.UnitTests
     public class EventTrackerTests
     {
         [Test]
-        public void CreateEmptyTracker()
+        public void AddEventToTracker()
         {
             //arrange
-            
-            var eventTracker = new EventTracker;
+            var eventList = new List<Event>();
+            var initEvent = EventBuilder
+                .Event(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now, "Title",
+                    0.5)
+                .Build();
+            eventList.Add(initEvent);
+            var eventForAdding = EventBuilder
+                .Event(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now, "Added Title",
+                    0.5)
+                .Build();
+
 
             //act
-
+            var eventTracker = CreateEventTracker(false, eventList);
+            eventTracker.AddEvent(eventForAdding);
+            
             //assert
+            Assert.That(eventTracker.Events, Is.EquivalentTo(new List<Event> {initEvent, eventForAdding}));
+        }
+        
+        public void RemoveEventFromTracker()
+        {
+            //arrange
+            var eventList = new List<Event>();
+            var initEvent = EventBuilder
+                .Event(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now, "Title",
+                    0.5)
+                .Build();
+            eventList.Add(initEvent);
+            var eventForAdding = EventBuilder
+                .Event(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now, "Added Title",
+                    0.5)
+                .Build();
+
+
+            //act
+            var eventTracker = CreateEventTracker(false, eventList);
+            eventTracker.RemoveEvent(eventForAdding);
+            
+            //assert
+            Assert.That(eventTracker.Events, Is.EquivalentTo(new List<Event> {initEvent, eventForAdding}));
         }
 
-        private void EventTracker(bool emptyOrNot)
+        private static EventTracker CreateEventTracker(bool empty, List<Event> eventList)
         {
-            emptyOrNot ? return EventTracker{
-                (Guid.NewGuid(), "Tracker Name", ) :
-            }
+            return new EventTracker(Guid.NewGuid(),
+                "name1",
+                eventList,
+                Guid.NewGuid()
+            );
         }
-        
-        
     }
 }
--- ItHappend/Domain/EventTracker.cs
+++ ItHappend/Domain/EventTracker.cs
@@ -28,5 +28,6 @@ namespace ItHappend.Domain
         {
             return Events.Remove(newEvent);
         }
+        
     }
 }
