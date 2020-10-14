b4a15e87d7daf076c335104c3c621efd6d54c51c
Nikita Elanskiy <nikitosbaik@mail.ru>
2020-10-14 22:53:28 +0300

    Add IFactStatistics.cs and MostFrequentEvent.cs

---
+++ ItHappend/Domain/IFactStatistics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItHappend.Domain
+{
+    public interface IFactStatistics
+    {
+        string Description { get; }
+        double Priority { get; }
+        
+        IFactStatistics ApplicabilityFunction(EventTracker[] events);
+    }
+}
