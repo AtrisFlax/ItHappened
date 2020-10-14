a660a21d4d2e259cdb23d71f73ffe078f9d03119
Roman Rubtsov <w-raven91@mail.ru>
2020-10-14 18:18:00 +0300

    Change UserService.cs: add constructor, change method signatures

--- ItHappend/Application/UserService/IUserService.cs
+++ ItHappend/Application/UserService/IUserService.cs
@@ -3,11 +3,10 @@ using ItHappend.Domain;
 
 namespace ItHappend.Application
 {
-    interface IUserService
+    public interface IUserService
     {
-        (Guid id, UserServiceStatusCodes status) RegisterUser(string login, string password, IPasswordHasher securePasswordHasher, IUserRepository userRepository);
+        public (Guid id, UserServiceStatusCodes status) RegisterUser(string login, string password);
 
-        (UserInfo userInfo, UserServiceStatusCodes status) AuthenticateUser(string login, string password, IUserRepository userRepository,
-            IEventTrackerRepository eventTrackerRepository, IPasswordHasher securePasswordHasher);
+        public (UserInfo userInfo, UserServiceStatusCodes status) AuthenticateUser(string login, string password);
     }
 }
