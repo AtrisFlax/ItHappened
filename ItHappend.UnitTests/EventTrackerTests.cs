fbac0d9578879a35128b60c8feb8c7ca8c8ab66a
Roman Rubtsov <w-raven91@mail.ru>
2020-10-14 19:00:16 +0300

    Add FilterEventsByTimeSpan tests

--- ItHappend.UnitTests/EventTrackerTests.cs
+++ ItHappend.UnitTests/EventTrackerTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using ItHappend.Domain;
 using NUnit.Framework;
 
@@ -17,7 +16,7 @@ namespace ItHappend.UnitTests
             var eventList = new List<Event> {initEvent};
 
             //act
-            var eventTracker = CreateEventTracker(false, eventList);
+            var eventTracker = CreateEventTracker(eventList);
             eventTracker.AddEvent(eventForAdding);
 
             //assert
@@ -33,13 +32,55 @@ namespace ItHappend.UnitTests
             var eventList = new List<Event>{firstEvent, secondEvent};
             
             //act
-            var eventTracker = CreateEventTracker(false, eventList);
+            var eventTracker = CreateEventTracker(eventList);
             eventTracker.RemoveEvent(secondEvent);
 
             //assert
             Assert.That(eventTracker.Events, Is.EquivalentTo(new List<Event> {firstEvent}));
         }
 
+        [Test]
+        public void FilterEventsByTimeSpan_ShouldReturnNoEvents()
+        {
+            //arrange
+            var eventList = new List<Event>();
+            var from = new DateTimeOffset(DateTime.Now);
+            var to = new DateTimeOffset(DateTime.Now + TimeSpan.FromDays(1));
+            var expected = 0;
+            
+            //act
+            var eventTracker = CreateEventTracker(eventList);
+            var filteredEvents = eventTracker.FilterEventsByTimeSpan(from, to);
+            var actual = filteredEvents.Count;
+            
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void FilterEventsByTimeSpan_ShouldFilterEvents()
+        {
+            //arrange
+            var firstEvent = CreateEvent("First");
+            firstEvent.HappensDate = new DateTimeOffset(2020, 10, 14, 18, 00, 00, TimeSpan.Zero).ToLocalTime();
+            var secondEvent = CreateEvent("Second");
+            secondEvent.HappensDate = new DateTimeOffset(2020, 10, 14, 18, 00, 59, TimeSpan.Zero).ToLocalTime();
+            var thirdEvent = CreateEvent("Third");
+            thirdEvent.HappensDate = new DateTimeOffset(2020, 10, 14, 17, 59, 59, TimeSpan.Zero).ToLocalTime();
+            var eventList = new List<Event> {firstEvent, secondEvent, thirdEvent};
+            var from = new DateTimeOffset(2020, 10, 14, 18, 00, 00, TimeSpan.Zero);
+            var to = new DateTimeOffset(2020, 10, 14, 18, 01, 00, TimeSpan.Zero);
+            var expected = 2;
+            
+            //act
+            var eventTracker = CreateEventTracker(eventList);
+            var filteredEvents = eventTracker.FilterEventsByTimeSpan(from, to);
+            var actual = filteredEvents.Count;
+            
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        
         private static Event CreateEvent(string tittle)
         {
             return EventBuilder
@@ -47,7 +88,7 @@ namespace ItHappend.UnitTests
                 .Build();
         }
 
-        private static EventTracker CreateEventTracker(bool empty, List<Event> eventList)
+        private static EventTracker CreateEventTracker(List<Event> eventList)
         {
             return new EventTracker(Guid.NewGuid(),
                 "name1",
