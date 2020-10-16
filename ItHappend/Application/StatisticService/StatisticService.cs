b3ae9556fa2e8516fd28364b6b45734397c0122c
Roman Rubtsov <w-raven91@mail.ru>
2020-10-16 10:27:31 +0300

    sketch CreateJsonReport func

--- ItHappend/Application/StatisticService/StatisticService.cs
+++ ItHappend/Application/StatisticService/StatisticService.cs
@@ -16,7 +16,7 @@ namespace ItHappend.StatisticService
 
 
         private StatisticService(IStatisticEngine statisticEngine, IUserRepository userRepository,
-            IEventTrackerRepository eventTrackerRepository, IJsonService jsonService, Guid userId)
+            IEventTrackerRepository eventTrackerRepository, IJsonService jsonService)
         {
             _statisticEngine = statisticEngine;
             _userRepository = userRepository;
@@ -26,21 +26,45 @@ namespace ItHappend.StatisticService
 
 
         //TODO Correct assembly of the log #issue 49
-        public string CreateJsonReport(IStatisticEngine statisticEngine, IUserRepository userRepository,
-            IEventTrackerRepository eventTrackerRepository, IJsonService jsonService, Guid userId,
-            IEnumerable<Guid> targetEventIds)
+        public string CreateJsonReport(Guid userId, IEnumerable<Guid> targetEventIds)
         {
             var result = new StringBuilder();
-            var trackers = eventTrackerRepository.LoadUserTrackers(userId);
+            var trackers = _eventTrackerRepository.LoadUserTrackers(userId);
+            //1. Сначала определяем значения общих фактов
+            //Общие факты вычисляются для всей коллекции трекеров
+            var mostCommonEvent = _statisticEngine.GetMostFrequentEventFact(trackers);
+            var nEventsHappened = _statisticEngine.GetOverallEventsNumber(trackers);
+            var dayWithLargestEventsCount = _statisticEngine.GetDatWithLargestEventNumber(trackers);
+            var weekWithLargestEventCount = _statisticEngine.GetWeekWithLargestEventNumber(trackers);
+            //Тут надо перепроверить как их объединять лучше
+            result.AppendJoin(',', new [] {mostCommonEvent, nEventsHappened, dayWithLargestEventsCount, weekWithLargestEventCount});
+
+            //2. Определяем значения специфичных фактов
+            foreach (var tracker in trackers)
+            {
+                //Тут для каждого трекера находим всевозможные специфичные факты, если для трекера факт не подходит,
+                //то нужно возвращать status code или null
+                var eventsNumberFact = _statisticEngine.GetNumberOfTrackerEvents(tracker);
+                var averageRatingFact = _statisticEngine.CreateAverageRatingFact(tracker);
+                var averageScaleFact = _statisticEngine.GetAverageScaleFact(tracker);
+                var sumOfScaleValuesFact = _statisticEngine.GetSumOfScaleValues(tracker);
+                var eventDidNotHappenLongTimeFact = _statisticEngine.GetDidNotHappenLongTimeEvent(tracker);
+                var eventHappensOnCertainWeekDaysFact = _statisticEngine.GetHappensOnCertainWeekDaysEvent(tracker);
+                var eventHappensAtCertainTimeOfDayFact = _statisticEngine.GetHappensAtCertainTimeOfDayEvent(tracker);
+                var worstEventFact = _statisticEngine.GetWorstEvent(tracker);
+                var bestEventFact = _statisticEngine.GetBestEvent(tracker);
+                var longestBreakFact = _statisticEngine.GetLongestBreak(tracker);
+            }
+            
             foreach (var eventId in targetEventIds)
             {
                 try
                 {
                     var targetTracker = trackers.First(tracker => tracker.Id == eventId);
-                    var averageRatingFact = statisticEngine.CreateAverageRatingFact(targetTracker);
+                    var averageRatingFact = _statisticEngine.CreateAverageRatingFact(targetTracker);
                     if (averageRatingFact.HasValue)
                     {
-                        var jsonString = jsonService.SerializeObject(statisticEngine);
+                        var jsonString = _jsonService.SerializeObject(_statisticEngine);
                         result.Append(jsonString);
                     }
                     else
