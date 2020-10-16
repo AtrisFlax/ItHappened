6665fd1dab9918b7a7127f45dc3be4b887c894c2
Roman Rubtsov <w-raven91@mail.ru>
2020-10-16 15:33:50 +0300

    create domain classes

---
+++ ItHappend/Domain/StatisticsEngine/SingleTrackerCalculator/WorstEventCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using ItHappend.Domain.EventCustomization;
+using ItHappend.Domain.Statistics.StatisticsFacts;
+using LanguageExt;
+using LanguageExt.UnsafeValueAccess;
+
+namespace ItHappend.Domain.Statistics.SingleTrackerCalculator
+{
+    public class WorstEventCalculator
+    {
+        public Option<ISingleTrackerStatisticsFact> Calculate(EventTracker eventTracker)
+        {
+            if (!CanCalculate(eventTracker)) return Option<ISingleTrackerStatisticsFact>.None;
+
+            var worstEvent = eventTracker.Events.OrderBy(eventItem => eventItem.Rating).First();
+            var priority = 10 - worstEvent.Rating.Value();
+            var description =
+                $"Событие в отслеживании {eventTracker.Name} с самым низким рейтингом {worstEvent.Rating} " +
+                $"произошло {worstEvent.HappensDate} с комментарием {worstEvent.Comment}";
+            var comment = worstEvent.Comment.ValueUnsafe();
+            return Option<ISingleTrackerStatisticsFact>.Some(new WorstEvent(description,
+                priority,
+                worstEvent.HappensDate,
+                comment,
+                worstEvent));
+        }
+
+        private bool CanCalculate(EventTracker eventTracker)
+        {
+            var isEventsNumberWithRatingMoreOrEqualToTen = eventTracker.Events
+                .Count(eventItem => eventItem.Rating.IsSome) >= 10;
+            var isOldestEventHappenedMoreThanThreeMonthsAgo = eventTracker.Events
+                                                                  .OrderBy(eventItem => eventItem.HappensDate)
+                                                                  .First().HappensDate <= DateTimeOffset.Now - TimeSpan.FromDays(90);
+            var isEventWithLowestRatingHappenedMoreThanWeekAgo = eventTracker.Events
+                                                                     .OrderBy(eventItem => eventItem.Rating)
+                                                                     .First().HappensDate <= DateTimeOffset.Now - TimeSpan.FromDays(7);
+            return isEventsNumberWithRatingMoreOrEqualToTen && 
+                   isOldestEventHappenedMoreThanThreeMonthsAgo &&
+                   isEventWithLowestRatingHappenedMoreThanWeekAgo;
+        }
+    }
+}
---
+++ ItHappend/Domain/StatisticsEngine/SingleTrackerStatisticsFacts/WorstEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ItHappend.Domain.EventCustomization;
+
+namespace ItHappend.Domain.Statistics.StatisticsFacts
+{
+    public class WorstEvent : ISingleTrackerStatisticsFact
+    {
+        public string Description { get; }
+        public double Priority { get; }
+        public DateTimeOffset Date { get; }
+        public Comment Comment { get; }
+        public Event EventReference { get; }
+        
+        public WorstEvent(string description, double priority, DateTimeOffset date, Comment comment, Event eventReference)
+        {
+            Description = description;
+            Priority = priority;
+            Date = date;
+            Comment = comment;
+            EventReference = eventReference;
+        }
+    }
+}
