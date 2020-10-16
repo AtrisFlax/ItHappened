4b222543d745487f8a42368fda89d492665459ef
Roman Rubtsov <w-raven91@mail.ru>
2020-10-16 14:18:00 +0300

    create EventService

--- Usage/CompositionRoot.cs
+++ Usage/CompositionRoot.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using ItHappend;
-using ItHappend.Domain;
+﻿using ItHappend.Application;
 using ItHappend.EventService;
 using ItHappend.EventTrackerService;
 using ItHappend.Infrastructure;
@@ -13,6 +11,7 @@ namespace Usage
         public IEventService EventService { get; private set; }
         public IEventTrackerService EventTrackerService { get; private set; }
         public IUserService UserService { get; private set; }
+
         public static CompositionRoot Create()
         {
             var userRepository = new UserRepository();
@@ -21,12 +20,10 @@ namespace Usage
             var passwordHasher = new PasswordHasher();
             return new CompositionRoot()
             {
-                //TODO event service init 
-                //EventService = new EventService(eventRepository),
-                UserService =  new UserService(userRepository, passwordHasher, eventTrackerRepository),
+                EventService = new EventService(eventRepository),
+                UserService = new UserService(userRepository, passwordHasher, eventTrackerRepository),
                 EventTrackerService = new EventTrackerService(eventTrackerRepository, userRepository, eventRepository)
             };
         }
-        
     }
 }
