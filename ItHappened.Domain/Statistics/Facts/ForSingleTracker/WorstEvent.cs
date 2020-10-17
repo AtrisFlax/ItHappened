c40ef8bc35ca9f6282cff8fd4a2c138aa7ec24e9
Roman Rubtsov <w-raven91@mail.ru>
2020-10-17 16:56:15 +0300

    Inserted files with WorstEvent

---
+++ ItHappened.Domain/Statistics/Facts/ForSingleTracker/WorstEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ItHappened.Domain;
+using ItHappened.Domain.EventCustomization;
+using ItHappened.Domain.Statistics.Facts;
+using LanguageExt;
+
+namespace ItHappend.Domain.Statistics.StatisticsFacts
+{
+    public class WorstEvent : IStatisticsFact
+    {
+        public string FactName { get; }
+        public string Description { get; }
+        public double Priority { get; }
+        public DateTimeOffset Date { get; }
+        public Option<Comment> Comment { get; }
+        public Event EventReference { get; }
+        
+        public WorstEvent(string factName, string description, double priority, DateTimeOffset date, Option<Comment> comment, Event eventReference)
+        {
+            FactName = factName;
+            Description = description;
+            Priority = priority;
+            Date = date;
+            Comment = comment;
+            EventReference = eventReference;
+        }
+    }
+}
