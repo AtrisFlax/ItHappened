36053864113fab7c6f53446ef9e11f0a2c0ac33e
steppam <steppam@mail.ru>
2020-10-17 13:30:10 +0300

    added SingleTrackerEventsCountFact + calc;

---
+++ ItHappened.Domain/Statistics/Facts/ForSingleTracker/SingleTrackerEventsCountFact.cs
@@ -0,0 +1,18 @@
+﻿namespace ItHappened.Domain.Statistics.Facts.ForSingleTracker
+{
+    public class SingleTrackerEventsCountFact : ISingleTrackerStatisticsFact
+    {
+        public SingleTrackerEventsCountFact(string factName, string description, double priority, int eventsCount)
+        {
+            FactName = factName;
+            Description = description;
+            Priority = priority;
+            EventsCount = eventsCount;
+        }
+
+        public string FactName { get; }
+        public string Description { get; }
+        public double Priority { get; }
+        public int EventsCount { get; }
+    }
+}
