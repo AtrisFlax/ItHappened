64aa6542af1120ec014d21103bd56a5b9b7526d4
Roman Rubtsov <w-raven91@mail.ru>
2020-10-23 14:19:10 +0300

    Create IGeneralFactsRepository.cs and ISpecificFactsRepository.cs interfaces

---
+++ ItHappened.Domain/Repositories/IGeneralFactsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using ItHappened.Domain.Statistics;
+
+namespace ItHappened.Domain
+{
+    public interface IGeneralFactsRepository
+    {
+        IReadOnlyCollection<IGeneralFact> LoadUserGeneralFacts(Guid userId);
+        void UpdateUserGeneralFacts(Guid userId);
+    }
+}
