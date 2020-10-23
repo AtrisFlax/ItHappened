8c23c27842d30e66a4501b10106eb74dde1b2df8
Pavel Sharapov <atrisflax@yandex.ru>
2020-10-23 16:33:41 +0300

    Fix filtration objects

---
+++ ItHappened.Domain/Filtration/DateTimeFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ItHappened.Domain
+{
+    public class DateTimeFilter : IEventsFilter
+    {
+        public string Name { get; }
+        public DateTimeOffset From { get; }
+        public DateTimeOffset To { get; }
+
+        public DateTimeFilter(string name, DateTimeOffset @from, DateTimeOffset to)
+        {
+            Name = name;
+            From = @from;
+            To = to;
+        }
+
+        public IReadOnlyCollection<Event> Filter(IReadOnlyCollection<Event> events)
+        {
+            return events
+                .Where(eventItem =>
+                    eventItem.HappensDate.UtcDateTime >= From.UtcDateTime &&
+                    eventItem.HappensDate.UtcDateTime <= To.UtcDateTime).ToList();
+        }
+    }
+}
