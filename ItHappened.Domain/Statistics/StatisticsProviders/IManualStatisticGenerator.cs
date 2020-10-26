fc99f69388862c514892f32f16160e4600faa217
Roman Rubtsov <w-raven91@mail.ru>
2020-10-26 22:11:59 +0300

    Add interfaces for manual and background statistic update

---
+++ ItHappened.Domain/Statistics/StatisticsProviders/IManualStatisticGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ItHappened.Domain.Statistics
+{
+    public interface IManualStatisticGenerator
+    {
+        public void UpdateOnRequestUserGeneralFacts(Guid userId);
+        public void UpdateOnRequestTrackerSpecificFacts(Guid trackerId);
+    }
+}    
