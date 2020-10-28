8e88b7aea3fc2a5966505dc827b3d9ffd560e5ba
Roman Rubtsov <w-raven91@mail.ru>
2020-10-28 21:12:34 +0300

    Complete EventServiceTests.cs and add check if tracker customization is required

--- ItHappened.UnitTests/ServicesTests/EventServiceTests.cs
+++ ItHappened.UnitTests/ServicesTests/EventServiceTests.cs
@@ -49,7 +49,58 @@ namespace ItHappened.UnitTests.ServicesTests
         }
         
         [Test]
-        public void CreateEventWhenEventAndTrackerCustomizationDiffers_ThrowsRestException()
+        public void CreateEventWhenEventAndTrackerCustomizationDiffersAndCustomizationIsRequired_ThrowsRestException()
+        {
+            var tracker = TestingMethods.CreateTrackerWithRequiredCustomization(Guid.NewGuid(), "tracker",
+                new TrackerCustomizationSettings(
+                    true, 
+                    true,
+                    Option<string>.Some("meter"), 
+                    true, 
+                    false, 
+                    true, 
+                    true));
+            _trackerRepository.SaveTracker(tracker);
+            
+            Assert.Throws<RestException>(() =>
+                _eventService.CreateEvent(tracker.CreatorId, tracker.Id, DateTimeOffset.Now,
+                    new EventCustomParameters()));
+        }
+        
+        [Test]
+        public void CreateEventWhenEventAndTrackerCustomizationEqualsAndCustomizationIsRequired_CreatesEvent()
+        {
+            var tracker = TestingMethods.CreateTrackerWithRequiredCustomization(Guid.NewGuid(), "tracker",
+                new TrackerCustomizationSettings(
+                    true, 
+                    true,
+                    Option<string>.Some("meter"), 
+                    false, 
+                    true, 
+                    false, 
+                    true));
+            _trackerRepository.SaveTracker(tracker);
+            var newEventCustomization = new EventCustomParameters(
+                Option<Photo>.Some(new Photo(photoBytes: new byte[5])),
+                Option<double>.Some(1),
+                Option<double>.None,
+                Option<GeoTag>.Some(new GeoTag(10, 20)),
+                Option<Comment>.None);
+            
+            var createdEventId = _eventService.CreateEvent(
+                tracker.CreatorId, 
+                tracker.Id, 
+                DateTimeOffset.Now,
+                newEventCustomization);
+            
+            Assert.True(_eventRepository.IsContainEvent(createdEventId));
+            Assert.AreEqual(newEventCustomization.GetHashCode(), 
+                _eventRepository.LoadEvent(createdEventId).CustomizationsParameters.GetHashCode());
+            Assert.True(tracker.IsUpdated);
+        }
+        
+        [Test]
+        public void CreateEventWhenEventAndTrackerCustomizationDiffersAndCustomizationIsNotRequired_CreatesEvent()
         {
             var tracker = TestingMethods.CreateTrackerWithRequiredCustomization(Guid.NewGuid(), "tracker",
                 new TrackerCustomizationSettings(
@@ -60,12 +111,15 @@ namespace ItHappened.UnitTests.ServicesTests
                     false, 
                     true, 
                     false));
+            _trackerRepository.SaveTracker(tracker);
             
-            Assert.Throws<RestException>(() =>
-                _eventService.CreateEvent(tracker.CreatorId, tracker.Id, DateTimeOffset.Now,
-                    new EventCustomParameters()));
+            var createdEventId = _eventService.CreateEvent(tracker.CreatorId, tracker.Id, DateTimeOffset.Now,
+                new EventCustomParameters());
+            
+            Assert.True(_eventRepository.IsContainEvent(createdEventId));
+            Assert.True(tracker.IsUpdated);
         }
-
+        
         [Test]
         public void GetEventWhenNoRequiredEventInRepository_ThrowsRestException()
         {
@@ -161,15 +215,86 @@ namespace ItHappened.UnitTests.ServicesTests
         }
         
         [Test]
-        public void EditEventWhenNewCustomizationDontMatchTrackerCustomization_ThrowsRestException()
+        public void EditEventWhenNewCustomizationDontMatchTrackerCustomizationAndCustomizationIsRequired_ThrowsRestException()
         {
-            //TODO сделать после merge. Возможно нужно два вида. Где Customization is required and not
+            var tracker = TestingMethods.CreateTrackerWithRequiredCustomization(Guid.NewGuid(), "tracker",
+                new TrackerCustomizationSettings(
+                    true, 
+                    true,
+                    Option<string>.Some("meter"), 
+                    true, 
+                    false, 
+                    true, 
+                    true));
+            _trackerRepository.SaveTracker(tracker);
+            var oldEvent = TestingMethods.CreateEvent(tracker.Id, tracker.CreatorId);
+            _eventRepository.SaveEvent(oldEvent);
+            
+            Assert.Throws<RestException>(() =>
+                _eventService.EditEvent(tracker.CreatorId, oldEvent.Id, DateTimeOffset.Now,
+                    new EventCustomParameters()));
+        }
+        
+        
+        [Test]
+        public void EditEventWhenNewCustomizationMatchTrackerCustomizationAndCustomizationIsRequired_EventInRepositoryUpdated()
+        {
+            var tracker = TestingMethods.CreateTrackerWithRequiredCustomization(Guid.NewGuid(), "tracker",
+                new TrackerCustomizationSettings(
+                    true, 
+                    true,
+                    Option<string>.Some("meter"), 
+                    false, 
+                    true, 
+                    false, 
+                    true));
+            _trackerRepository.SaveTracker(tracker);
+            var oldEvent = new Event(Guid.NewGuid(), tracker.CreatorId, tracker.Id, DateTimeOffset.Now, 
+                new EventCustomParameters(
+                    Option<Photo>.Some(new Photo(photoBytes: new byte[5])),
+                    Option<double>.Some(1),
+                    Option<double>.None,
+                    Option<GeoTag>.Some(new GeoTag(10, 20)),
+                    Option<Comment>.None));
+            _eventRepository.SaveEvent(oldEvent);
+            
+            var updatedCustomizationParameters = new EventCustomParameters(
+                Option<Photo>.Some(new Photo(photoBytes: new byte[7])),
+                Option<double>.Some(15),
+                Option<double>.None,
+                Option<GeoTag>.Some(new GeoTag(30, 40)),
+                Option<Comment>.None);
+            
+            _eventService.EditEvent(tracker.CreatorId, oldEvent.Id, DateTimeOffset.Now,
+                updatedCustomizationParameters);
+            
+            Assert.AreEqual(updatedCustomizationParameters.GetHashCode(), 
+                _eventRepository.LoadEvent(oldEvent.Id).CustomizationsParameters.GetHashCode());
+            Assert.True(tracker.IsUpdated);
         }
         
         [Test]
-        public void EditEventGoodCase_RequiredEventUpdatedInRepository()
+        public void EditEventWhenNewCustomizationDontMatchTrackerCustomizationAndCustomizationNotRequired_EventInRepositoryUpdated()
         {
-            //TODO сделать после merge
+            var tracker = TestingMethods.CreateTrackerWithRequiredCustomization(Guid.NewGuid(), "tracker",
+                new TrackerCustomizationSettings(
+                    true, 
+                    true,
+                    Option<string>.Some("meter"), 
+                    true, 
+                    false, 
+                    true, 
+                    false));
+            _trackerRepository.SaveTracker(tracker);
+            var oldEvent = TestingMethods.CreateEvent(tracker.Id, tracker.CreatorId);
+            _eventRepository.SaveEvent(oldEvent);
+
+            _eventService.EditEvent(tracker.CreatorId, oldEvent.Id, DateTimeOffset.Now,
+                new EventCustomParameters());
+            
+            Assert.AreNotEqual(oldEvent.GetHashCode(), 
+                _eventRepository.LoadEvent(oldEvent.Id).GetHashCode());
+            Assert.True(tracker.IsUpdated);
         }
 
         [Test]
@@ -201,37 +326,5 @@ namespace ItHappened.UnitTests.ServicesTests
             Assert.False(_eventRepository.IsContainEvent(_event.Id));
             Assert.True(_tracker.IsUpdated);
         }
-        
-        //TODO проверить после merge, т.к. функция IsSettingsAndEventCustomizationsMatch написана в этой ветке с логическими ошибками
-        // [Test]
-        // public void CreateEventGoodCase_CreatedEventSavedInRepository()
-        // {
-        //     var tracker = TestingMethods.CreateTrackerWithRequiredCustomization(Guid.NewGuid(), "tracker",
-        //         new TrackerCustomizationSettings(
-        //             Option<string>.Some("meter"), 
-        //             true, 
-        //             false, 
-        //             true, 
-        //             false, 
-        //             true, 
-        //             false));
-        //     _trackerRepository.SaveTracker(tracker);
-        //     var happensDateOfNewEvent = DateTimeOffset.Now;
-        //     var newEventCustomization = new EventCustomParameters(
-        //         Option<Photo>.Some(new Photo(photoBytes: new byte[5])),
-        //         Option<double>.Some(1),
-        //         Option<double>.None,
-        //         Option<GeoTag>.Some(new GeoTag(10, 20)),
-        //         Option<Comment>.None);
-        //     var createdEventId = _eventService.CreateEvent(
-        //         tracker.CreatorId,
-        //         tracker.Id, happensDateOfNewEvent,
-        //         newEventCustomization);
-        //
-        //     var eventFromRepository = _eventRepository.LoadEvent(createdEventId);
-        //
-        //     Assert.AreEqual(newEventCustomization.GetHashCode(), eventFromRepository.CustomizationsParameters.GetHashCode());
-        //     Assert.AreEqual(happensDateOfNewEvent, eventFromRepository.HappensDate);
-        // }
     }
 }
