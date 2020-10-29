a2d3cc375a27c07eabf3c10bb3c1f0e8c73cb5a0
Roman Rubtsov <w-raven91@mail.ru>
2020-10-29 23:44:08 +0300

    Migrations

---
+++ ItHappened.Infrastructure/Migrations/20201029192701_AddGeoTag.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItHappened.Infrastructure.Migrations
+{
+    public partial class AddGeoTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GeoTags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    GpsLat = table.Column<double>(nullable: false),
+                    GpsLng = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeoTags", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GeoTags");
+        }
+    }
+}
