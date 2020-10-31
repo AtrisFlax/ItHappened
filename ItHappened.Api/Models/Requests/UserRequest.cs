b937014849c660f2724107e8677acc29c024279f
Paul <atrisflax@yandex.ru>
2020-10-31 15:54:38 +0300

    Delete UserRequest.cs

--- ItHappened.Api/Models/Requests/UserRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace ItHappened.Api.Models.Requests
-{
-    public class UserRequest
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-    
-}
