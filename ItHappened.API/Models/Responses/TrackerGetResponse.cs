5c2bca36868cf2101815afffdc29a21baa7e3237
Pavel Sharapov <atrisflax@yandex.ru>
2020-10-31 13:55:58 +0300

    Remove responses clases

--- ItHappened.API/Models/Responses/TrackerGetResponse.cs
+++ ItHappened.API/Models/Responses/TrackerGetResponse.cs
@@ -1,11 +1,11 @@
-﻿using System;
-
-namespace ItHappened.Api.Models.Responses
-{
-    public class TrackerGetResponse
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public CustomizationSettingsResponse CustomizationSettings { get; set; }
-    }
-}
+﻿// using System;
+//
+// namespace ItHappened.Api.Models.Responses
+// {
+//     public class TrackerGetResponse
+//     {
+//         public Guid Id { get; set; }
+//         public string Name { get; set; }
+//         public CustomizationSettingsResponse CustomizationSettings { get; set; }
+//     }
+// }
