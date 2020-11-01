cd8e4da7c8bd4f0f2c4f3d9506d46ddf43be017d
Roman Rubtsov <w-raven91@mail.ru>
2020-11-01 14:07:38 +0300

    remove nullable from TrackerDto and make mapping for it

--- ItHappened.Infrastructure/Mappers/EventTrackerDto.cs
+++ ItHappened.Infrastructure/Mappers/EventTrackerDto.cs
@@ -8,13 +8,15 @@ namespace ItHappened.Infrastructure.Mappers
         public Guid CreatorId { get; set; }
 
         public string Name { get; set; }
+        public string ScaleMeasurementUnit { get; set; }
 
-        public bool? IsPhotoRequired { get; set; }
-        public bool? IsScaleRequired { get; set; }
-        public bool? IsRatingRequired { get; set; }
-        public bool? IsGeotagRequired { get; set; }
-        public bool? IsCommentRequired { get; set; }
-        public bool? IsCustomizationRequired { get; set; }
+        //TODO спросить у Паши зачем nullable
+        public bool IsPhotoRequired { get; set; }
+        public bool IsScaleRequired { get; set; }
+        public bool IsRatingRequired { get; set; }
+        public bool IsGeotagRequired { get; set; }
+        public bool IsCommentRequired { get; set; }
+        public bool IsCustomizationRequired { get; set; } 
         
         public UserDto UserDto { get; set; }
     }
