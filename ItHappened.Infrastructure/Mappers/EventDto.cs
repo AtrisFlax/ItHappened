e857af79c3e7beacb89d7fe66dda77e3c0ac3952
Roman Rubtsov <w-raven91@mail.ru>
2020-11-02 00:33:23 +0300

    Make EventDto to Event mapper; make properties of double type nullable in EventDto.cs

--- ItHappened.Infrastructure/Mappers/EventDto.cs
+++ ItHappened.Infrastructure/Mappers/EventDto.cs
@@ -11,10 +11,10 @@ namespace ItHappened.Infrastructure.Mappers
         public DateTimeOffset HappensDate { get; set; }
 
         public byte[] Photo { get; set; }
-        public double Scale { get; set; }
-        public double Rating { get; set; }
-        public double LatitudeGeo { get; set; }
-        public double LongitudeGeo { get; set; }
+        public double? Scale { get; set; }
+        public double? Rating { get; set; }
+        public double? LatitudeGeo { get; set; }
+        public double? LongitudeGeo { get; set; }
         public string Comment { get; set; }
         
         //public EventTrackerDto EventTrackerDto { get; set; }
