965912030df6da21f86b9f5e4c2990156895204b
Roman Rubtsov <w-raven91@mail.ru>
2020-11-01 18:00:51 +0300

    Create EventTrackerDto to ToEventTracker mapper class

--- ItHappened.Infrastructure/Mappers/DBToDomainMappingProfiles.cs
+++ ItHappened.Infrastructure/Mappers/DBToDomainMappingProfiles.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using ItHappened.Domain;
+using LanguageExt;
+using Microsoft.AspNetCore.Routing.Constraints;
 
 namespace ItHappened.Infrastructure.Mappers
 {
@@ -8,6 +10,23 @@ namespace ItHappened.Infrastructure.Mappers
         public DbToDomainMappingProfiles()
         {
             CreateMap<UserDto, User>();
+            CreateMap<EventTrackerDto, EventTracker>().ConvertUsing<EventTrackerDtoToEventTrackerConverter>();
+
+        }
+    }
+
+    public class EventTrackerDtoToEventTrackerConverter : ITypeConverter<EventTrackerDto, EventTracker>
+    {
+        public EventTracker Convert(EventTrackerDto source, EventTracker destination, ResolutionContext context)
+        {
+            var scaleMeasurementUnit = source.ScaleMeasurementUnit.IsNull() ? 
+                Option<string>.None : Option<string>.Some(source.ScaleMeasurementUnit);
+            
+            var tracker = new EventTracker(source.Id, source.CreatorId, source.Name,
+                new TrackerCustomizationSettings(source.IsPhotoRequired, source.IsScaleRequired, scaleMeasurementUnit,
+                    source.IsRatingRequired, source.IsGeotagRequired, source.IsCommentRequired,
+                    source.IsCustomizationRequired));
+            return tracker;
         }
     }
 }
