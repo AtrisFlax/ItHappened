cbe85a17165dffdd2e8ccdb471aa9276cd464b94
steppam <steppam@mail.ru>
2020-11-02 05:50:06 +0300

    fix.

--- ItHappened.API/Middleware/ExceptionResponse.cs
+++ ItHappened.API/Middleware/ExceptionResponse.cs
@@ -1,15 +1,18 @@
-﻿﻿using System.Collections.Generic;
+﻿﻿using System;
+ using System.Collections.Generic;
 
 namespace ItHappened.Api.Middleware
 {
     public class ExceptionResponse
     {
-        public ExceptionResponse(string errorMessage, Dictionary<string, object> payload = null)
+        public ExceptionResponse(string errorMessage, string exceptionType = null, Dictionary<string, object> payload = null)
         {
             ErrorMessage = errorMessage;
+            ExceptionType = exceptionType;
             Payload = payload;
         }
-
+        
+        public string ExceptionType { get; }
         public string ErrorMessage { get; }
         public Dictionary<string, object> Payload { get; }
     }
